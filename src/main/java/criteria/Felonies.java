fb88136e91509d0201937760ab4c2055431740c7
Michael Andon <andon.m3@gmail.com>
2024-09-11 02:26:51 -0700

    chore: remove unneeded period package

--- src/main/java/criteria/Felonies.java
+++ src/main/java/criteria/Felonies.java
@@ -5,7 +5,6 @@ import models.Decision;
 import models.Results;
 
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
