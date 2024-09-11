e26aa9a675adf24e3fa0f2d042e41850849b9ad9
Michael Andon <andon.m3@gmail.com>
2024-09-11 02:05:50 -0700

    chore: refactor felonies to include dates and validation check; also use setters

--- src/main/java/models/Applicant.java
+++ src/main/java/models/Applicant.java
@@ -1,5 +1,9 @@
 package models;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Applicant {
     String firstName;
     String lastName;
@@ -8,19 +12,21 @@ public class Applicant {
     double gpaScale;
     int satScore;
     int actScore;
-    int felonies;
+    boolean felonies;
+    List<LocalDate> felonyDates = new ArrayList<>();
     String state;
 
-    public Applicant(String firstName, String lastName, int age, double gpa, double gpaScale, int satScore, int actScore, int felonies, String state) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.gpa = gpa;
-        this.gpaScale = gpaScale;
-        this.satScore = satScore;
-        this.actScore = actScore;
-        this.felonies = felonies;
-        this.state = state;
+    public Applicant(String firstName, String lastName, int age, double gpa, double gpaScale, int satScore, int actScore, boolean felonies, List<String> felonyDates, String state) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+        setGpa(gpa);
+        setGpaScale(gpaScale);
+        setSatScore(satScore);
+        setActScore(actScore);
+        setFelonies(felonies);
+        setFelonyDates(felonyDates);
+        setState(state);
     }
 
     public String getFirstName() {
@@ -79,12 +85,26 @@ public class Applicant {
         this.actScore = actScore;
     }
 
-    public int getFelonies() {
+    public boolean getFelonies() {
         return felonies;
     }
 
-    public void setFelonies(int felonies) {
+    public void setFelonies(boolean felonies) {
         this.felonies = felonies;
+    }
+
+    public List<LocalDate> getFelonyDates() {
+        return felonyDates;
+    }
+
+    public void setFelonyDates(List<String> felonyDates) {
+        for (String felonyDate : felonyDates) {
+            // Parse the date string to a LocalDate object
+            LocalDate givenDate = LocalDate.parse(felonyDate);
+
+            this.felonyDates.add(givenDate);
+        }
+
     }
 
     public String getState() {
